0caaeacb042a390ba13351a83001c8e44702ee50
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-24 13:50:54 +0100

    adds event listener for click on pending

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 /* Exercise 1: Wish list */
 function addToList(item) {
     $('#items').append('<li>' + item + '<span class="label pending">Pending</span>' + '</li>');    
-    $('#item').append('pending')
 }
 
 $(document).on('click', '#add-to-list', function() {
     addToList($("#item").val());
     $('#item').val('');
     $('#item').focus();
+})
+
+
+
+$(document).on('click', '#items', function() {
+    var li_node = $(this).parent();
+    li_node.append('<span class="label success">Done!</span>');
+    li_node.remove('<span class="label pending">Pending</span>');
 })
