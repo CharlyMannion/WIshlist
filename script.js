f36fa8eea0c2110198010026e6f2fdc2a74960d4
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-25 11:24:44 +0100

    removes pending label

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /* Exercise 1: Wish list */
+
 function addToList(item) {
-    $('#items').append('<li>' + item + '<span class="label pending">Pending</span>' + '</li>');    
+    var status_label = '<span class="label pending">Pending</span>'
+    $('#items').append('<li>' + item + status_label + '</li>');    
 }
 
 $(document).on('click', '#add-to-list', function() {
@@ -11,8 +13,8 @@ $(document).on('click', '#add-to-list', function() {
 
 $(document).on('click', '.pending', function() {
     var li_node = $(this).parent();
-    // li_node.remove('<span class="label pending">Pending</span>');
     li_node.append('<span class="label success">Done!</span>');
     li_node.addClass('completed');
+    $('.pending').remove();
 })
 
