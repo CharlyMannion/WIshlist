e4c142f8ef60acd8b1af2f2b33186ccc73a3331e
Charlotte Mannion <charlottemannion100@gmail.com>
2019-10-26 12:40:04 +0100

    wraps javascript in function

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 /* Exercise 1: Wish list */
 
-function addToList(item) {
-    var status_label = '<span class="label pending">Pending</span>'
-    $('#items').append('<li>' + item + status_label + '</li>');    
-}
+$(document).ready(function() {
 
-$(document).on('click', '#add-to-list', function() {
-    addToList($("#item").val());
-    $('#item').val('');
-    $('#item').focus();
-})
+    var pending = $('.pending').length;
+    var completed = $('.completed').length;
+    $('.total').text('Pending: ');
 
-$(document).on('click', '.pending', function() {
-    var li_node = $(this).parent();
-    li_node.append('<span class="label success">Done!</span>');
-    li_node.addClass('completed');
-    li_node.remove('<span class="label pending">Pending</span>');
-    // $('.pending').remove();
-})
+    function addToList(item) {
+        var status_label = '<span class="label pending">Pending</span>'
+        $('#items').append('<li>' + item + status_label + '</li>');    
+    }
 
+    $(document).on('click', '#add-to-list', function() {
+        addToList($("#item").val());
+        $('#item').val('');
+        $('#item').focus();
+    })
+
+    $(document).on('click', '.pending', function() {
+        var li_node = $(this).parent();
+        li_node.append('<span class="label success">Done!</span>');
+        li_node.addClass('completed');
+        // li_node.remove('<span class="label pending">Pending</span>');
+        // $('.pending').remove();
+    })
+
+});
